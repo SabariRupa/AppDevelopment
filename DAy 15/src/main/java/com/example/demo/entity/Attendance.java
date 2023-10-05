e31626e935b6ea8a03ee9f42ac55b4f7fc37b266
SabariRupa <mohanrajmano1973@gmail.com>
2023-10-05 14:21:54 +0530

    Day  14 and 15

---
+++ DAy 15/src/main/java/com/example/demo/entity/Attendance.java
@@ -0,0 +1,27 @@
+package com.example.demo.entity;
+
+import java.util.Date;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Attendance 
+{
+	  @Id
+	  private int student_id;
+	  private Date date;
+	  private int hour;
+	  private String value;
+	  
+}
